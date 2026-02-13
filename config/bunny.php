342833587fb05a26a4d626323531a3daaad0607d
Joris Noordermeer <joris@noordermeer.ch>
2026-02-13 16:39:15 +0100

    build: add compiled assets, update gitignore, and rename env variables to BUNNY_STREAM prefix

--- config/bunny.php
+++ config/bunny.php
@@ -2,13 +2,13 @@
 return [
     // The unique identifier for your Bunny Stream library.
     // This is required to interact with the Bunny Stream API and manage your videos.
-    'library_id' => env('BUNNY_LIBRARY_ID'),
+    'library_id' => env('BUNNY_STREAM_LIBRARY_ID'),
 
     // The hostname for your Bunny CDN, used for streaming videos.
     // Typically, this is a custom domain or a Bunny.net-provided URL.
-    'hostname' => env('BUNNY_CDN_HOSTNAME'),
+    'hostname' => env('BUNNY_STREAM_CDN_HOSTNAME'),
 
     // The API key for authenticating requests to the Bunny Stream API.
     // This should be kept secure and not exposed in frontend code.
-    'api_key' => env('BUNNY_API_KEY'),
+    'api_key' => env('BUNNY_STREAM_API_KEY'),
 ];
