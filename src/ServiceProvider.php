5cf1e53de144a089c81463fd60f9beb551eb9f67
Joris Noordermeer <joris@noordermeer.ch>
2026-02-16 13:57:28 +0100

    fix: simplify config publish tag from statamic-bunny-stream-config to bunny-stream-config

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -37,6 +37,6 @@ class ServiceProvider extends AddonServiceProvider
 
         $this->publishes([
             __DIR__ . '/../config/bunny-stream.php' => config_path('statamic/bunny-stream.php'),
-        ], 'statamic-bunny-stream-config');
+        ], 'bunny-stream-config');
     }
 }
