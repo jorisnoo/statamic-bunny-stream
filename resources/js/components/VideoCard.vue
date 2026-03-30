18eb944fa367493bcfc2c5c74bfb73d399e8b41b
Joris Noordermeer <joris@noordermeer.ch>
2026-03-30 13:26:21 +0800

    feat: add thumbnail cache bust functionality

--- resources/js/components/VideoCard.vue
+++ resources/js/components/VideoCard.vue
@@ -3,9 +3,37 @@
         <td>
             <div class="tw:flex tw:items-center tw:gap-2">
                 <template v-if="currentVideo.status >= 4">
-                    <a :href="videoUrl" target="_blank" class="tw:shrink-0">
-                        <img :src="thumbnailUrl" class="tw:size-16 tw:rounded tw:object-cover" />
-                    </a>
+                    <div class="tw:relative tw:shrink-0 tw:group">
+                        <a :href="videoUrl" target="_blank">
+                            <img :src="thumbnailUrl" class="tw:size-16 tw:rounded tw:object-cover" />
+                        </a>
+                        <button
+                            v-if="!isUploadingThumbnail"
+                            @click.prevent="selectThumbnail"
+                            class="tw:absolute tw:inset-0 tw:flex tw:items-center tw:justify-center tw:bg-black/50 tw:rounded tw:opacity-0 tw:group-hover:opacity-100 tw:transition-opacity tw:cursor-pointer"
+                            :title="__('Select new thumbnail')"
+                        >
+                            <svg class="tw:size-5 tw:text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909M3.75 21h16.5A2.25 2.25 0 0 0 22.5 18.75V5.25A2.25 2.25 0 0 0 20.25 3H3.75A2.25 2.25 0 0 0 1.5 5.25v13.5A2.25 2.25 0 0 0 3.75 21Z" />
+                            </svg>
+                        </button>
+                        <div
+                            v-if="isUploadingThumbnail"
+                            class="tw:absolute tw:inset-0 tw:flex tw:items-center tw:justify-center tw:bg-black/50 tw:rounded"
+                        >
+                            <svg class="tw:size-5 tw:animate-spin tw:text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                <circle class="tw:opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
+                                <path class="tw:opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z" />
+                            </svg>
+                        </div>
+                        <input
+                            ref="thumbnailInput"
+                            type="file"
+                            accept="image/jpeg,image/png"
+                            class="tw:hidden"
+                            @change="uploadThumbnail"
+                        />
+                    </div>
                     <template v-if="!isEditing">
                         <a :href="videoUrl" target="_blank" class="tw:text-sm tw:font-medium tw:truncate">
                             {{ currentVideo.title }}
@@ -90,12 +118,19 @@ export default {
         return {
             currentVideo: this.video,
             isLoading: false,
-            thumbnailUrl: `https://${this.bunnyHostname}/${this.video.guid}/${this.video.thumbnailFileName}`,
             videoUrl: `https://iframe.mediadelivery.net/play/${this.video.videoLibraryId}/${this.video.guid}`,
             triggerDeletion: false,
             isEditing: false,
             editTitle: '',
+            isUploadingThumbnail: false,
+            thumbnailCacheBuster: '',
         }
+    },
+    computed: {
+        thumbnailUrl() {
+            const base = `https://${this.bunnyHostname}/${this.currentVideo.guid}/${this.currentVideo.thumbnailFileName}`;
+            return this.thumbnailCacheBuster ? `${base}?v=${this.thumbnailCacheBuster}` : base;
+        },
     },
     mounted() {
         if (this.currentVideo.status < 4) {
@@ -211,6 +246,51 @@ export default {
         },
         cancelEditing() {
             this.isEditing = false;
+        },
+        selectThumbnail() {
+            this.$refs.thumbnailInput.click();
+        },
+        uploadThumbnail(event) {
+            const file = event.target.files[0];
+            if (!file) return;
+
+            this.isUploadingThumbnail = true;
+
+            fetch(`https://video.bunnycdn.com/library/${this.bunnyLibrary}/videos/${this.currentVideo.guid}/thumbnail`, {
+                method: 'POST',
+                headers: {
+                    AccessKey: this.bunnyApiKey,
+                    'Content-Type': file.type,
+                },
+                body: file,
+            })
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error('Failed to upload thumbnail');
+                    }
+
+                    this.thumbnailCacheBuster = Date.now();
+                    this.loadVideo();
+                    this.bustThumbnailCache();
+
+                    Statamic.$toast.success(__('Thumbnail has been updated!'));
+                })
+                .catch((error) => {
+                    console.error(error);
+                    Statamic.$toast.error(__('An error occured while trying to update the thumbnail.'));
+                })
+                .finally(() => {
+                    this.isUploadingThumbnail = false;
+                    this.$refs.thumbnailInput.value = '';
+                });
+        },
+        bustThumbnailCache() {
+            fetch(`/cp/bunny/thumbnail/${this.currentVideo.guid}/bust`, {
+                method: 'POST',
+                headers: {
+                    'X-CSRF-TOKEN': Statamic.$config.get('csrfToken'),
+                },
+            }).catch((error) => console.error('Cache bust failed:', error));
         },
     }
 }
--- routes/cp.php
+++ routes/cp.php
@@ -6,3 +6,4 @@ use Noo\BunnyStream\Http\Controllers\Cp\Overview;
 Route::get('/bunny/videos', Overview::class)->name('bunny.cp.videoBrowser');
 Route::get('/bunny/videos/list', \Noo\BunnyStream\Http\Controllers\Cp\VideoList::class)->name('bunny.cp.videoList');
 Route::get('/bunny/thumbnail/{guid}', \Noo\BunnyStream\Http\Controllers\Cp\Thumbnail::class)->name('bunny.cp.thumbnail');
+Route::post('/bunny/thumbnail/{guid}/bust', \Noo\BunnyStream\Http\Controllers\Cp\BustThumbnailCache::class)->name('bunny.cp.thumbnailBust');
